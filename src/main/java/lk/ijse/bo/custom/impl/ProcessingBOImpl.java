24dd46cbabd842cf786df0128bd955df889c35d3
Induwara <induwara2k@gmail.com>
2024-01-07 23:50:39 +0530

    fixed issue in processeing bo impl

--- src/main/java/lk/ijse/bo/custom/impl/ProcessingBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/ProcessingBOImpl.java
@@ -4,31 +4,38 @@ import lk.ijse.bo.BOFactory;
 import lk.ijse.bo.custom.ProcessingBO;
 import lk.ijse.bo.custom.TeaBookTypeBO;
 import lk.ijse.bo.custom.TeaTypeBO;
+import lk.ijse.dao.DAOFactory;
+import lk.ijse.dao.custom.TeaBookTypeDAO;
+import lk.ijse.dao.custom.TeaTypeDAO;
 import lk.ijse.db.DbConnection;
 import lk.ijse.dto.TeaBookTypeDetailDto;
+import lk.ijse.entity.TeaBookTypeDetails;
 import lk.ijse.util.TransactionUtil;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProcessingBOImpl implements ProcessingBO {
 
-    private final TeaBookTypeBO teaBookTypeBO = (TeaBookTypeBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.TEA_BOOK_TYPE);
-    private final TeaTypeBO teaTypeBO = (TeaTypeBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.TEA_TYPE);
+
+    private final TeaBookTypeDAO teaBookTypeDAO = (TeaBookTypeDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.TEA_BOOK_TYPE);
+
+    private final TeaTypeDAO teaTypeDAO = (TeaTypeDAO) DAOFactory.getInstance().getDAO(DAOFactory.DAOTypes.TEA_TYPE);
 
 
     @Override
     public boolean updateDetails(LocalDate date, List<TeaBookTypeDetailDto> dtoList) throws SQLException {
-
         boolean result = false;
         try{
             TransactionUtil.autoCommitFalse();
-            boolean isConfirmed = teaBookTypeBO.confirmTeaBook(date);
+
+            boolean isConfirmed = teaBookTypeDAO.confirmTeaBook(date);
 
             if (isConfirmed){
-                boolean isUpdated = teaTypeBO.updateTeaTypeAmount(dtoList);
+                boolean isUpdated = updateTeaTypeAmount(dtoList);
 
                 if (isUpdated){
                     TransactionUtil.commit();
@@ -41,5 +48,23 @@ public class ProcessingBOImpl implements ProcessingBO {
 
         return result;
     }
+
+    public boolean updateTeaTypeAmount(List<TeaBookTypeDetailDto> dtoList) throws SQLException {
+        List<TeaBookTypeDetails> teaBookTypeDetailsList = new ArrayList<>();
+        for (TeaBookTypeDetailDto dto : dtoList) {
+            teaBookTypeDetailsList.add(new TeaBookTypeDetails(
+                    dto.getTypeId(),dto.getAmount()
+            ));
+        }
+        for (TeaBookTypeDetails teaBookTypeDetails : teaBookTypeDetailsList) {
+            boolean isUpdated=teaTypeDAO.updateTeaTypeAmount(teaBookTypeDetails);
+            if(!isUpdated){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
 
